f6910987b948ca450ecd0e0276bdc27eca1a9eef
Likeapro <iamlikeaproforyou@gmail.com>
2024-02-14 01:16:22 +0530

    Completed the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+      <div className="login-container">
+        <div className="methods">
+          <button>GITHUB</button>
+          <button>GOOGLE</button>
+          <button>FACEBOOK</button>
+        </div>
+        <div >
+          <form action="" className='form'>
+            <input type="text" placeholder='Enter email' name='username'/>
+            <input type="text" placeholder='Enter password' name='password'/>
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
